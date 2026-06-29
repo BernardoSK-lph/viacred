4866742740c27d4ec6f9312103109c5fce1338c9
BernardoSK-lph <bernardo@leadsperhour.com>
2026-06-29 17:50:55 -0300

    Update App.jsx

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import logo from "./assets/logo.png";
+import desenho from "./assets/desenho.png"
 
 export default function App() {
 
   return (
     <div>
       <img src={logo} className="logo" />
+      <img src={desenho} className="desenho" />
     </div>
   );
 }
